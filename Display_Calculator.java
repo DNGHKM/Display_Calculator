d8209b4f43488cfb20011a5eaaa1571a495db844
gozip <134900901+DNGHKM@users.noreply.github.com>
2023-07-29 23:55:24 +0900

    ver2

--- Display_Calculator.java
+++ Display_Calculator.java
@@ -1,13 +1,24 @@
 import java.util.Scanner;
-
-/**
- * Display_Calculator
- */
 public class Display_Calculator {
-        public static double width_ratio;
-        public static double height_ratio;
-        public static double diagonal;
-      
+    public static double width_ratio;
+    public static double height_ratio;
+    public static double diagonal;
+    public static double width_cm;
+    public static double height_cm;
+    public static double width_inch_to_cm(){
+        return 2.54*width_ratio*Math.sqrt((diagonal*diagonal)/(width_ratio*width_ratio+height_ratio*height_ratio));
+    }
+    public static double height_inch_to_cm(){
+        return 2.54*height_ratio*Math.sqrt((diagonal*diagonal)/(width_ratio*width_ratio+height_ratio*height_ratio));    
+    }
+    public static void Notice_Display(double width_cm, double height_cm) {
+        System.out.println("*******************");
+        System.out.println("가로 길이 ="+width_cm+"cm");
+        System.out.println("세로 길이 ="+height_cm+"cm");
+        System.out.println("면적(넓이) = "+width_cm*height_cm+"cm^2");
+        System.out.println("*******************");
+    }
+
     public static void main(String[] args) {
         try (Scanner in = new Scanner(System.in)) {
         while (true) {
@@ -17,23 +28,13 @@ public class Display_Calculator {
             height_ratio=in.nextFloat();
         System.out.println("인치를 입력하세요");    
             diagonal=in.nextFloat();
-        double width_inch = (double) ((double) width_ratio*Math.sqrt((diagonal*diagonal)/(width_ratio*width_ratio+height_ratio*height_ratio)));
-        double height_inch = (double) ((double) height_ratio*Math.sqrt((diagonal*diagonal)/(width_ratio*width_ratio+height_ratio*height_ratio)));    
-
-        double width_cm = width_inch*2.54;
-        double height_cm = (double) (height_inch*2.54);
-
       
-        Notice_Display(width_cm, height_cm);
+        double width = width_inch_to_cm();
+        double height = height_inch_to_cm();
+        Notice_Display(width, height);
     }
     }    
     }
 
-    public static void Notice_Display(double width_cm, double height_cm) {
-        System.out.println("-----------------------------");
-        System.out.println("가로 길이 ="+width_cm+"cm");
-        System.out.println("세로 길이 ="+height_cm+"cm");
-        System.out.println("면적(넓이) = "+width_cm*height_cm+"cm^2");
-        System.out.println("-----------------------------");
-    }
+
 }
